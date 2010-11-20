56fcf8bfd36f7faa96e84495765536f7e0257e56
Elsendoorn <elsendoorn@w3craft.nl>
2010-11-20 22:48:06 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tele2V01::Application.config.secret_token = 'aadc2c2c3321b39ad48c022f1be800d96ed4424f2f909846a22718d289a96071806a4e62dd54ffab7d15bd5a3233b46bb2f8cf7d58e99e8e3169e6d78d0f9114'
